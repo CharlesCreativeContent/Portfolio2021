ad07248b46494e18846a762e61a21748f1064da7
Shawn Charles <62077627+CharlesCreativeContent@users.noreply.github.com>
2024-10-10 02:51:03 -0400

    Add files via upload

--- snowstorm.js
+++ snowstorm.js
@@ -42,7 +42,7 @@ var snowStorm = (function(window, document) {
   this.flakeWidth = 8;            // Max pixel width reserved for snow element
   this.flakeHeight = 8;           // Max pixel height reserved for snow element
   this.vMaxX = 6;                 // Maximum X velocity range for snow
-  this.vMaxY = 4;                 // Maximum Y velocity range for snow
+  this.vMaxY = 1;                 // Maximum Y velocity range for snow
   this.zIndex = 0;                // CSS stacking order applied to each snowflake
 
   // --- "No user-serviceable parts inside" past this point, yadda yadda ---
